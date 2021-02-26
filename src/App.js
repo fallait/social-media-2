fa2391c5cfdf95a15f45212df0c79a500896ef5a
Glitch (starter-cra-and-express) <none>
2021-02-26 23:46:50 +0000

    ✂️😋 Checkpoint ./src/App.js:58163/150 ./src/utils/publicUrl.js:58163/98 ./src/Navbar.js:58163/748 ./jsconfig.json:58163/269 ./.glitch-assets:58163/8367 ./src/index.css:58163/131 ./src/Navbar.module.css:58163/542

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import css from './App.module.css';
+import Header from './Header.js';
+import Home from './Home.js';
+import Navbar from './Navbar.js';
 
 function App() {
   return (
--- src/Navbar.js
+++ src/Navbar.js
@@ -8,12 +8,12 @@ function Navbar() {
         <nav className={css.navbar}>
             <div className={css.navItem}>
                 <button>
-                    <img src={publicUrl('/assets/home.svg')} alt="Home"/>
+                    <img src='https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Fhome.svg?v=1614382900187' alt="Home"/>
                 </button>
             </div>
             <div className={css.navItem}>
                 <button>
-                    <img src={publicUrl('/assets/explore.svg')} alt="Explore"/>
+                    <img src='https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Fexplore.svg?v=1614382900067' alt="Explore"/>
                 </button>
             </div>
 						...
