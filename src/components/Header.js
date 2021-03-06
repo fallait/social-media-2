22f1665c0eb020436052f6b3fe70f381b3b6ca94
Glitch (starter-cra-and-express) <none>
2021-03-06 02:24:07 +0000

    🎣🍇 Checkpoint ./src/components/Header.js:58163/1158 ./src/components/Header.module.css:58163/1453 ./src/components/Navbar.js:58163/1522 ./src/components/App.module.css:58163/79

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+// import publicUrl from 'utils/publicUrl';
+import css from './Header.module.css';
 function Header() {
   return (
-    <h2>
-      Header    
-    </h2>
+    <header className={css.header}>
+        <div>
+          <button>
+            <img src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Fcamera.svg?v=1614382899995" alt="camera"/>
+          </button>
+        </div>
+        <div>
+          <img src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Flogo.png?v=1614382900226" alt="logo"/>
+        </div>
+        <div>
+          <button>
+          <img src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Fcomment.svg?v=1614382900046" alt="message"/>
+          </button>
+        </div>
+        
+    </header>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,21 @@ function Navbar() {
                     <img src='https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Fexplore.svg?v=1614382900067' alt="Explore"/>
                 </button>
             </div>
-						...
+						            <div className={css.navItem}>
+                <button>
+                    <img src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Fnewpost.svg?v=1614382900337" alt="New Post"/>
+                </button>
+            </div>
+            <div className={css.navItem}>
+                <button>
+                    <img src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Flike.svg?v=1614382900207" alt="Activity"/>
+                </button>
+            </div>
+            <div className={css.navItem}>
+                <button>
+                    <img src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Fprofile.svg?v=1614382900375" alt="Profile"/>
+                </button>
+            </div>
         </nav>
     );
 }
