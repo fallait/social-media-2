11c966951357e1c2fe93cb6d62ca4c0d6eacc91b
Glitch (starter-cra-and-express) <none>
2021-03-21 23:49:58 -0400

    Updates

---
+++ precache-manifest.ba5221c98b1e91ee54a951cb295227db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a44b15c718206dba6be5cf56ec2ed0",
+    "url": "/social-media-2/index.html"
+  },
+  {
+    "revision": "9bcd72be095cb03df722",
+    "url": "/social-media-2/static/css/main.99b2a16e.chunk.css"
+  },
+  {
+    "revision": "9a5a6f92f17425caf0b6",
+    "url": "/social-media-2/static/js/2.c369f712.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/social-media-2/static/js/2.c369f712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bcd72be095cb03df722",
+    "url": "/social-media-2/static/js/main.2c61f0a5.chunk.js"
+  },
+  {
+    "revision": "04ce130c05bd821ebd54",
+    "url": "/social-media-2/static/js/runtime-main.79e3cbe9.js"
+  }
+]);
